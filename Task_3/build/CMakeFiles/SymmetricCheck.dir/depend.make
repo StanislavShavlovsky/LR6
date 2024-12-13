6b8a4394569426931aa6f47eb09946d07724f76c
Stasname <97255058+StanislavShavlovsky@users.noreply.github.com>
2024-12-13 08:22:16 +0300

    Delete Task_3 directory

--- Task_3/build/CMakeFiles/SymmetricCheck.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SymmetricCheck.
-# This may be replaced when dependencies are built.
--- Task_3/symmetric_check.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYMMETRIC_CHECK_H
-#define SYMMETRIC_CHECK_H
-
-#include <string>
-
-bool is_symmetric(const std::wstring &str);
-
-#endif 
